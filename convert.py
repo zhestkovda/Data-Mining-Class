e986f8b782b139e2b2159c0b78c484c0aee08a12
HarmonicSeries <tatiana.gaponova@gmail.com>
2015-05-26 18:39:30 +0300

    stuff

--- convert.py
+++ convert.py
@@ -11,9 +11,11 @@ while (len(line)>0):
     someone = line.split()
     i=someone[0]
     print i
-    followers = ' '.join(someone[1:])
+    followers = someone[1:]
     f_nodes.write(i+'\n')
-    f_edges.write(followers+'\n')
+
+    for follower in followers:
+    	f_edges.write(i+' '+follower+'\n')
     line = f.readline()
 
 f.close()
